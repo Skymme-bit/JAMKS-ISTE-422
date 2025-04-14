a220cfdd87f4f88f57d1640ba2e0e62b69b24e5c
Jade V. <jade.vaillant@insa-rennes.fr>
2025-04-14 17:56:45 -0400

    validation tested

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    '^.+\\.tsx?$': ['ts-jest', {}],
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
   },
 };
