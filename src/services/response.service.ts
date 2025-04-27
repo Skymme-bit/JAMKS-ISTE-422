db935fa4335f372d92cf26ae36eb7ef237662205
Jade V. <jade.vaillant@insa-rennes.fr>
2025-04-27 18:35:10 -0400

    corrections

--- src/services/response.service.ts
+++ src/services/response.service.ts
@@ -1,6 +1,6 @@
 const responseWrapper = (status: string, event_type: string, body: any) => {
   // Returning the result of the function
   return { status: status, event: event_type, body };
-};
+}
 
 export default responseWrapper;
