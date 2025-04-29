59d58112efc96ab814562fadf892d32d661de05a
Jade V. <jade.vaillant@insa-rennes.fr>
2025-04-28 23:19:38 -0400

    solve bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
+// eslint.config.mjs
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import globals from "globals";
 
-
-export default defineConfig([
+export default tseslint.config([
+  ...tseslint.configs.recommended, // recommended config first
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: globals.node,
+    },
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.node }
-  },
-  tseslint.configs.recommended
+  }
 ]);
